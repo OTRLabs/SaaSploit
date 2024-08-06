18c14ed366a529f3e01a9aa3e3e072aadba56975
Cam <cammclain@skiff.com>
2024-08-06 11:49:16 -0500

    chore: Add PDM lock file and update dependencies

---
+++ src/saasploit/application.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+
+
+from rich.console import Console
+
+
+
+
+async def application_runner() -> None:
+    console: Console = Console()
+
+    console.print("")
+
+
+
+
+
+
+
+
+
+
---
+++ src/saasploit/configs/config.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+from rich.console import Console
+import os
+from dotenv import load_dotenv
+
+
+
+
+class Config:
+    SQLITE_DATABASE_URL: str = 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
