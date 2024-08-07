bcb7f676b4ff3f89961d4e9ed11060792f756b3f
Cam <cammclain@skiff.com>
2024-08-06 20:40:11 -0500

    chore: Refactor main.py for better code structure and remove unnecessary imports

--- src/main.py
+++ src/main.py
@@ -1,21 +1,13 @@
 from __future__ import annotations
-
 from rich.console import Console
 from datetime import datetime
-
-
-
-
-
-
+import asyncio
 
 async def main() -> None:
-    
     console: Console = Console()
-    
     console.print(f"Launching SaaSploit - {datetime.now()}")
 
 
+if __name__ == "__main__":
 
-
-
+    asyncio.run(main())
