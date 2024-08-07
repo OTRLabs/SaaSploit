42d61ceeea31d58133ff716485fb6e0783adc61d
Cam <cammclain@skiff.com>
2024-08-06 21:16:55 -0500

    chore: Update application_runner function to display TUI dashboard for analysis

--- src/saasploit/application.py
+++ src/saasploit/application.py
@@ -14,10 +14,9 @@ from datetime import datetime
 async def application_runner(console: Console, scope: str) -> None:
 
     
-    console.print(f"Launching Textual TUI dashboard for analysis of scope: {scope} - {datetime.now()}")
-
-    console.print(f"")
-
+    console.print(f"Launching Textual TUI dashboard for analysis of scope: {scope} - {datetime.now()}\n\n")
+    
+    console.print(f"Extracting scope from directory provided\n\n")
     console.clear()
     
     
