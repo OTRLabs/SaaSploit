96e1f7f7b415b892edb5275d408defb8e8a75ad7
Cam <cammclain@skiff.com>
2024-08-06 19:23:33 -0500

    chore: Add lazy loading for images in main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,21 @@
 from __future__ import annotations
 
-from
+from rich.console import Console
+from datetime import datetime
+
+
+
+
+
+
+
+async def main() -> None:
+    
+    console: Console = Console()
+    
+    console.print(f"Launching SaaSploit - {datetime.now()}")
+
+
+
+
+
